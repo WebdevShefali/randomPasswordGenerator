a921ed9b1d2414a72947b10b1f03c333dba4a88b
WebdevShefali <105041861+WebdevShefali@users.noreply.github.com>
2022-07-08 15:00:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function genPassword(){
+  let chars = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*()?";
+  var passwordLength = 8;
+  var password = "";
+  for(var i = 0; i<=passwordLength; i++){
+    var randomNumber = Math.floor(Math.random()*chars.length);
+    password += chars.substring(randomNumber, randomNumber+1);
+    document.getElementById("password").value = password;
+  }
+}
+
+function copy(){
+var copyText = document.getElementById("password");
+copyText.select();
+document.execCommand("copy");
+
+}
